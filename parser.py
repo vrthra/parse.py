314722b85e0f8d0c4eccb3d5c9b9ca6c7bcd7119
Rahul Gopinath <rahul@gopinath.org>
2017-11-10 19:49:19 -0800

    updates

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import functools
 import re
 import collections
 import json
@@ -26,14 +27,7 @@ term_grammar = {
 
 RE_NONTERMINAL = re.compile(r'(\$[a-zA-Z_]*)')
 
-class ParseResult:
-    def __str__(self):
-        if self.val == None: return self.key
-        return '(' + ''.join([str(i) for i in self.val]) + ')'
-
-    def __repr__(self):
-        if self.val == None: return self.key
-        return '<%s %s>' % (self.key, ''.join([repr(i) for i in self.val]))
+class ParseResult: pass
 
 class NoParse(ParseResult):
     def __bool__(self): return False
@@ -41,53 +35,55 @@ class NoParse(ParseResult):
 class KeyMatch(ParseResult):
     def __init__(self, key, frm, til, val): self.key, self.frm, self.til, self.val = key, frm, til, val
 
+    def __str__(self):
+        if self.val == None: return self.key
+        return '(' + self.key + ' ' + ''.join([str(i) for i in self.val]) + ')'
+
+    def to_json(self):
+        return {self.key : [i.to_json() for i in self.val] if self.val else [] }
+
 class RuleMatch(ParseResult):
     def __init__(self, val, till): self.val, self.till = val, till
 
+    def __str__(self):
+        return '[' + ''.join([str(i) for i in self.val]) + ']'
+
+    def to_json(self):
+        return [i.to_json() for i in self.val]
+
 def is_symbol(x): return x[0] == '$'
 
-def memoize(targnum):
-    def fn_wrap(function):
-        memo = {}
-        def wrapper(*args):
-            lst = (args[i] for i in targnum)
-            if lst in memo: return memo[lst]
-            rv = function(*args)
-            memo[lst] = rv
-            return rv
-        return wrapper
-    return fn_wrap
+class PEGParser:
+    def __init__(self, grammar): self.grammar = grammar
 
-def unify_line(rule, grammar, text, tfrom):
-    parts = [s for s in re.split(RE_NONTERMINAL, rule) if s]
-    results = []
-    while parts:
-        part, *parts = parts
-        if is_symbol(part):
-            res = unify_key(part, grammar, text, tfrom)
-            if not res: return RuleMatch(val=[], till=0)
-            results.append(res)
-            tfrom = res.til
-        else:
-            if text[tfrom:].startswith(part):
-                till = tfrom + len(part)
-                results.append(KeyMatch(part, tfrom, till, None))
-                tfrom = till
-            else: return RuleMatch(val=[], till=0)
-    return RuleMatch(val=results, till=tfrom)
+    @functools.lru_cache(maxsize=None)
+    def unify_line(self, rule, text, tfrom):
+        parts = [s for s in re.split(RE_NONTERMINAL, rule) if s]
+        results = []
+        while parts:
+            part, *parts = parts
+            if is_symbol(part):
+                res = self.unify_key(part, text, tfrom)
+                if not res: return RuleMatch(val=[], till=0)
+                results.append(res)
+                tfrom = res.til
+            else:
+                if text[tfrom:].startswith(part):
+                    till = tfrom + len(part)
+                    results.append(KeyMatch(part, tfrom, till, None))
+                    tfrom = till
+                else: return RuleMatch(val=[], till=0)
+        return RuleMatch(val=results, till=tfrom)
 
-
-# returns (key, from, till,val)}
-@memoize((0,2,3))
-def unify_key(key, grammar, text, tfrom=0):
-    rules = grammar[key]
-
-    # PEG -- try by order.
-    for rule in rules:
-        ret = unify_line(rule, grammar, text, tfrom)
-        if not ret.val: continue # PEG
-        return KeyMatch(key, tfrom, ret.till, ret.val)
-    return NoParse()
+    @functools.lru_cache(maxsize=None)
+    def unify_key(self, key, text, tfrom=0):
+        rules = self.grammar[key]
+        # PEG -- try by order.
+        for rule in rules:
+            ret = self.unify_line(rule, text, tfrom)
+            if not ret.val: continue # PEG
+            return KeyMatch(key, tfrom, ret.till, ret.val)
+        return NoParse()
 
 def using(fn):
     with fn as f: yield f
@@ -99,8 +95,8 @@ def main(args):
     if len(args) > 2:
         grammarstr, = [f.read().strip() for f in using(open(args[2], 'r'))]
         grammar = json.loads(grammarstr)
-    result = unify_key('$START', grammar, to_parse)
-    print(result)
+    result = PEGParser(grammar).unify_key('$START', to_parse)
+    print(json.dumps(result.to_json()))
 
 if __name__ == '__main__':
     main(sys.argv)
