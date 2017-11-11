aae113b3e6abb1b35fb5441bfff6dba28ce8c872
Rahul Gopinath <rahul@gopinath.org>
2017-11-11 13:28:52 -0800

    a top down CFG parser with mutually recursive procedures

--- parser.py
+++ parser.py
@@ -5,59 +5,52 @@ import re
 import json
 
 term_grammar = {
-    "$START":
-        ["$EXPR"],
-
-    "$EXPR":
-        ["$TERM + $EXPR", "$TERM - $EXPR", "$TERM"],
-    
-    "$TERM":
-        ["$FACTOR * $TERM", "$FACTOR / $TERM", "$FACTOR"],
-    
-    "$FACTOR":
-        ["+$FACTOR", "-$FACTOR", "($EXPR)", "$INTEGER.$INTEGER", "$INTEGER"],
-
-    "$INTEGER":
-        ["$DIGIT$INTEGER", "$DIGIT"],
-
-    "$DIGIT":
-        ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-}
+        "$START": ["$EXPR"],
+        "$EXPR": ["$TERM", "$TERM + $EXPR", "$TERM - $EXPR"],
+        "$TERM": ["$FACTOR", "$FACTOR * $TERM", "$FACTOR / $TERM"],
+        "$FACTOR": ["$INTEGER", "+$FACTOR", "-$FACTOR", "($EXPR)", "$INTEGER.$INTEGER"],
+        "$INTEGER": ["$DIGIT", "$DIGIT$INTEGER"],
+        "$DIGIT": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        }
 
 RE_NONTERMINAL = re.compile(r'(\$[a-zA-Z_]*)')
+def is_literal(v): return v[0] != '$'
 
-class PEGParser:
+class Parser:
     def __init__(self, grammar):
         def split(rule): return tuple(s for s in re.split(RE_NONTERMINAL, rule) if s)
         self.grammar = {k:[split(l) for l in rules] for k,rules in grammar.items()}
 
     def literal_match(self, part, text, tfrom):
-        return (tfrom + len(part), (part, [])) if text[tfrom:].startswith(part) else (tfrom, None)
+        return (tfrom + len(part), part) if text[tfrom:].startswith(part) else None
 
-    @functools.lru_cache(maxsize=None)
-    def unify_key(self, key, text, tfrom=0):
-        rules = self.grammar[key]
-        rets = (self.unify_line(rule, text, tfrom) for rule in rules)
-        tfrom, res = next((ret for ret in rets if ret[1] is not None), (tfrom, None))
-        return (tfrom, (key, res) if res is not None else None)
+    def unify_words(self, parts, text, tfrom):
+        part, *parts = parts
+        for l, p in self.unify_key(part, text, tfrom):
+            if not parts:
+                yield l, p
+            else:
+                for l1, s1 in self.unify_words(parts, text, l):
+                    yield l1, (p, s1)
 
-    @functools.lru_cache(maxsize=None)
-    def unify_line(self, parts, text, tfrom):
-        def is_symbol(v): return v[0] == '$'
-
-        results = []
-        for part in parts:
-            tfrom, res = (self.unify_key if is_symbol(part) else self.literal_match)(part, text, tfrom)
-            if res is None: return tfrom, None
-            results.append(res)
-        return tfrom, results
+    def unify_key(self, part, text, tfrom=0):
+        if is_literal(part):
+            val = self.literal_match(part, text, tfrom)
+            if val:
+                yield val
+            else:
+                raise StopIteration
+        else:
+            for rule in self.grammar[part]:
+                for l, p in self.unify_words(rule, text, tfrom):
+                    yield l, (part, p)
 
 def main(args):
     def readall(fn): return ''.join([f for f in open(fn, 'r')]).strip()
 
-    to_parse = readall(args[1])
+    to_parse = '12 + 34 * 4' #readall(args[1])
     grammar = json.loads(readall(args[2])) if len(args) > 2 else term_grammar
-    result = PEGParser(grammar).unify_key('$START', to_parse)
-    print(result[1])
+    for result in Parser(grammar).unify_key('$START', to_parse):
+        print(result)
 
 if __name__ == '__main__': main(sys.argv)
