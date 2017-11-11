63726d4fa483427e664740436a9a4fdfb0ec2ad6
Rahul Gopinath <rahul@gopinath.org>
2017-11-11 09:57:03 -0800

    update

--- parser.py
+++ parser.py
@@ -28,9 +28,8 @@ RE_NONTERMINAL = re.compile(r'(\$[a-zA-Z_]*)')
 
 class PEGParser:
     def __init__(self, grammar):
-        self.grammar = grammar
-        for k, rules in grammar.items():
-            grammar[k] = [tuple(s for s in re.split(RE_NONTERMINAL, rule) if s) for rule in rules]
+        def split(rule): return tuple(s for s in re.split(RE_NONTERMINAL, rule) if s)
+        self.grammar = {k:[split(l) for l in rules] for k,rules in grammar.items()}
 
     def literal_match(self, part, text, tfrom):
         return (tfrom + len(part), (part, [])) if text[tfrom:].startswith(part) else (tfrom, None)
@@ -40,7 +39,7 @@ class PEGParser:
         rules = self.grammar[key]
         rets = (self.unify_line(rule, text, tfrom) for rule in rules)
         tfrom, res = next((ret for ret in rets if ret[1] is not None), (tfrom, None))
-        return (tfrom, (key, res)) if res is not None else (tfrom, None)
+        return (tfrom, (key, res) if res is not None else None)
 
     @functools.lru_cache(maxsize=None)
     def unify_line(self, parts, text, tfrom):
@@ -48,23 +47,17 @@ class PEGParser:
 
         results = []
         for part in parts:
-            tfrom, res = self.unify_key(part, text, tfrom) if is_symbol(part) else self.literal_match(part, text, tfrom)
+            tfrom, res = (self.unify_key if is_symbol(part) else self.literal_match)(part, text, tfrom)
             if res is None: return tfrom, None
             results.append(res)
         return tfrom, results
 
-def using(fn):
-    with fn as f: yield f
-
 def main(args):
-    to_parse, = [f.read().strip() for f in using(open(args[1], 'r'))]
+    def readall(fn): return ''.join([f for f in open(fn, 'r')]).strip()
 
-    grammar = term_grammar
-    if len(args) > 2:
-        grammarstr, = [f.read().strip() for f in using(open(args[2], 'r'))]
-        grammar = json.loads(grammarstr)
+    to_parse = readall(args[1])
+    grammar = json.loads(readall(args[2]) if len(args) > 2 else term_grammar)
     result = PEGParser(grammar).unify_key('$START', to_parse)
     print(result[1])
 
-if __name__ == '__main__':
-    main(sys.argv)
+if __name__ == '__main__': main(sys.argv)
